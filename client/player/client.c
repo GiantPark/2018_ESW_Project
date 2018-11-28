852f8c03917159cedfd9b504549cb9495f9213dc
SeongHyeon Park <zkfthfl123@naver.com>
2018-11-28 02:21:09 -0800

    client.c update, Makefile update

--- client/player/client.c
+++ client/player/client.c
@@ -1,1 +1,22 @@
 //This is a client file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include "client.h"
+
+#define MAXLINE     1000
+#define NAME_LEN    20
+
+int main ()
+{
+	return 0;
+}

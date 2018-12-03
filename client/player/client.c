90d278d2c98e92710e9bed0474527d4c5c697038
SeongHyeon Park <zkfthfl123@naver.com>
2018-12-03 03:42:32 -0800

    client.c lobby add

--- client/player/client.c
+++ client/player/client.c
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]) {
 	int maxfdp1;	// 최대 소켓 디스크립터
 	int s;		// 소켓
 	int namelen;	// 이름의 길이
+	int roomflag = 0;
 	fd_set read_fds;
 	time_t ct;
 	struct tm tm;
@@ -42,6 +43,9 @@ int main(int argc, char *argv[]) {
 		errquit("tcp_connect fail");
 
 	puts("서버에 접속되었습니다.");
+	puts("commend list ---");
+        puts("방 참가 : in (방 번호)");
+        puts("방 만들기 : mk (방 제목)");
 	maxfdp1 = s + 1;
 	FD_ZERO(&read_fds);
 
@@ -56,20 +60,36 @@ int main(int argc, char *argv[]) {
 				bufmsg[nbyte] = 0;
 				write(1, "\033[0G", 4);		//커서의 X좌표를 0으로 이동
 				printf("%s", bufmsg);		//메시지 출력
-				fprintf(stderr, "\033[1;32m");	//글자색을 녹색으로 변경
-				fprintf(stderr, "%s>", argv[3]);//내 닉네임 출력
-
+				if (!strcmp(bufmsg,"goin\n")) roomflag = 1;
+				if (roomflag)
+				{
+					fprintf(stderr, "\033[1;32m");//글자색을 녹색으로 변경
+					fprintf(stderr, "%s>", argv[3]);//내 닉네임 출력
+				}
+				else
+				{
+					fprintf(stderr, "\033[1;37m");//글자색을 white으로 변경
+					fprintf(stderr,"lobby>");
+				}
 			}
 		}
 		if (FD_ISSET(0, &read_fds)) {
 			if (fgets(bufmsg, MAXLINE, stdin)) {
-				fprintf(stderr, "\033[1;33m"); //글자색을 노란색으로 변경
-				fprintf(stderr, "\033[1A"); //Y좌표를 현재 위치로부터 -1만큼 이동
-				ct = time(NULL);	//현재 시간을 받아옴
-				tm = *localtime(&ct);
-				sprintf(bufall, "[%02d:%02d:%02d]%s>%s", tm.tm_hour, tm.tm_min, tm.tm_sec, argv[3], bufmsg);//메시지에 현재시간 추가
-				if (send(s, bufall, strlen(bufall), 0) < 0)
-					puts("Error : Write error on socket.");
+				if (roomflag)
+				{
+					fprintf(stderr, "\033[1;33m"); //글자색을 노란색으로 변경
+					fprintf(stderr, "\033[1A"); //Y좌표를 현재 위치로부터 -1만큼 이동
+					ct = time(NULL);	//현재 시간을 받아옴
+					tm = *localtime(&ct);
+					sprintf(bufall, "[%02d:%02d:%02d]%s>%s", tm.tm_hour, tm.tm_min, tm.tm_sec, argv[3], bufmsg);//메시지에 현재시간 추가
+					if (send(s, bufall, strlen(bufall), 0) < 0)
+						puts("Error : Write error on socket.");
+				}
+				else
+				{
+					if (send(s, bufmsg, strlen(bufmsg), 0) < 0)
+						puts("Error : Write error on socket.");
+				}
 				if (strstr(bufmsg, EXIT_STRING) != NULL) {
 					puts("Good bye.");
 					close(s);
@@ -79,3 +99,4 @@ int main(int argc, char *argv[]) {
 		}
 	} // end of while
 }
+
